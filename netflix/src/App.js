184d478fbbf6dab4f483020fc9b1af7301f44bc0
vistritpandey <vistritpandey@gmail.com>
2020-10-08 17:20:09 +0530

    f

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>there</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey there lets make the Netflix Clone</h1>
+      <Row title ="Netflix Originals" />
+      <Row title = "Trending Now"/>
     </div>
   );
 }
