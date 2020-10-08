09fe0e2e62255d19bd10e4e8e1e7c71f75960f48
vistritpandey <vistritpandey@gmail.com>
2020-10-08 18:34:39 +0530

    g

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -25,6 +25,7 @@ function Row({ title, fetchUrl }) {
         {/*several rows*/}
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__posters"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
