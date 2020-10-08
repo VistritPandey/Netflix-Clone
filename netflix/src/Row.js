523bd303e94246be1c691c581328d8f9f1d22a72
vistritpandey <vistritpandey@gmail.com>
2020-10-08 18:18:47 +0530

    f

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,26 +1,39 @@
-import React,{ useState, useEffect} from 'react'
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        //
-        async function fetchData(){
-            const requests = await axios.get(fetchUrl);
-            console.log(requests);
-            return requests;
-        }
-        fetchData();
-    }, []);
+  useEffect(() => {
+    //
+    async function fetchData() {
+      const requests = await axios.get(fetchUrl);
+      setMovies(requests.data.results);
+      return requests;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
 
-    return (
-        <div>
-            <h2>{title}</h2>
-
-            {/*container*/}
-        </div>
-    )
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {/*several rows*/}
+        {movies.map((movie) => (
+          <img
+            className="row__posters"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/*container*/}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
