328cdb2525ea99f410fd1437210b247d2f5f4600
vistritpandey <vistritpandey@gmail.com>
2020-10-08 18:42:20 +0530

    F

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './App.css';
-import Row from './Row'
-import requests from './Requests';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./Requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone OP</h1>
-      <Row title ="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title = "Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
     </div>
   );
 }
